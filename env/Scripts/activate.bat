8a8d852ecd67c66dbb3351b7e89108aebef55867
Kushal L Gowda <kushallgowda97@gmail.com>
2024-12-02 16:37:54 +0530

    change env folder

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Prerana\OneDrive\Desktop\Major_project\project\env
+set VIRTUAL_ENV=C:\Users\Kushal L Gowda\Desktop\carbon_emissions\env
 
 if not defined PROMPT set PROMPT=$P$G
 
